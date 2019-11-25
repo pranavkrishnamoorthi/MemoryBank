2528132f1b31658a3e8a44085abadecacaec0d16
Pranav Krishnamoorthi <pranav01krishna@gmail.com>
2019-11-24 18:45:37 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: 'variables.env' });
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(5000, () => console.log('Express server is listening on port 5000'));
+
+const verifyWebhook = require('./verify-webhook');
+app.get('/', verifyWebhook);
+
+const messageWebhook = require('./message-webhook');
+app.post('/', messageWebhook);
